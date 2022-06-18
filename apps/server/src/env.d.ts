a22256485b220f50a336cbe7a012389a685bf15b
Jacob Thomas <jacobthomas0898@gmail.com>
2022-06-17 22:40:07 -0400

    chore(server): run gen-env to update .env.example and env.d.ts

--- apps/server/src/env.d.ts
+++ apps/server/src/env.d.ts
@@ -1,18 +1,20 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      PORT: string;
+      SERVER_PORT: string;
       DB_HOST: string;
       DB_PORT: string;
-      DEV_DB: string;
-      TEST_DB: string;
-      PROD_DB: string;
       DB_USERNAME: string;
       DB_PASSWORD: string;
-      EMAIL_USER: string;
-      EMAIL_PASSWORD: string;
+      DEV_DB: string;
+      TEST_DB_USER: string;
+      TEST_DB_PASSWORD: string;
+      TEST_DB: string;
+      PROD_DB: string;
       EMAIL_HOST: string;
       EMAIL_PORT: string;
+      EMAIL_USER: string;
+      EMAIL_PASSWORD: string;
       SESSION_SECRET: string;
       FRONTEND_URL: string;
     }
